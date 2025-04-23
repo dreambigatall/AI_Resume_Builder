f3a95bc8dbbfbfac741ad100da08f71505fbb529
khalid <halidmahammed.16.hm@gmail.com>
2025-04-24 00:02:05 +0300

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -54,6 +54,11 @@ app.use(async (req, res, next) => {
   }
   next();
 });
+
+app.use((req, res, next) => {
+  console.log(`🔄 Incoming request: ${req.method} ${req.url}`);
+  next();
+});
 // Routes
 app.use('/api/resumes', resumeRoutes);
 
